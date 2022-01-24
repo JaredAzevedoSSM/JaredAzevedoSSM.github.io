8c415a74ccdc1a8b39689050d32705a91808ca35
JaredAzevedoSSM <jaredssm@stanford.edu>
2022-01-23 16:25:04 -0800

    Update to Nav

---
+++ mainScript.js
@@ -0,0 +1,26 @@
+function toggleOpacity(){
+    if(window.matchMedia('(max-width:991px)').matches){
+        $(".navbar").css("background-color", "rgba(105, 20, 14, 0.9)");
+
+        $(document).scroll(function(event){
+            $(".navbar").css("background-color", "rgba(105, 20, 14, 0.9)");
+        });
+    }
+    else{
+        if($(document).scrollTop() < ($(".parallax").height() - $(".navbar").height())){
+            $(".navbar").css("background-color", "rgba(255, 255, 255, 0)");
+        }
+
+        $(document).scroll(function(event){
+            if($(document).scrollTop() > ($(".parallax").height() - $(".navbar").height())){
+                $(".navbar").css("background-color", "rgba(105, 20, 14, 0.9)");
+            }
+            else{
+                $(".navbar").css("background-color", "rgba(255, 255, 255, 0)");
+            }
+        });
+    }
+}
+
+$(toggleOpacity);
+$(window).resize(toggleOpacity);
