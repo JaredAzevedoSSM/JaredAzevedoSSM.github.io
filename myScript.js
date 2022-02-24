2dcaba08d012530e488ccaaadb7fedfa7ee4b972
JaredAzevedoSSM <jaredssm@stanford.edu>
2022-02-24 12:17:10 -0800

    V0.87
    Changed document name???

--- myScript.js
+++ myScript.js
@@ -25,5 +25,29 @@ function toggleOpacity(){
     //}
 }
 
+function scrollTrigger(selector){
+    let els = document.querySelectorAll(selector);
+    els = Array.from(els);
+
+    els.forEach(el => {
+        addObserver(el, options);
+    })
+}
+
+function addObserver(el, options){
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('active')
+                observer.unobserve(entry.target)
+            }
+        })
+    })
+
+    observer.observe(el)
+}
+
+scrollTrigger('.scroll-animate', {rootMargin:"-200px"})
+
 $(toggleOpacity);
 $(window).resize(toggleOpacity);
